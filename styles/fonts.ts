a0b974c03438c0ba210c31ba424eca741570461f
bekione <kinber024@gmail.com>
2024-12-06 15:47:13 -0800

    added ubuntu font

--- styles/fonts.ts
+++ styles/fonts.ts
@@ -1,13 +1,29 @@
 // styles/fonts.ts
 import localFont from "next/font/local";
 
+export const techniqNormal = localFont({
+  src: "../app/fonts/techniqu.ttf",
+  variable: "--font-techniq-u",
+  weight: "100 900",
+});
+
 export const techniqOutlined = localFont({
   src: "../app/fonts/techniqo.ttf",
   variable: "--font-techniq-o",
   weight: "100 900",
 });
 
-https://www.linkedin.com/company/adot-tech
+export const ubuntuReg = localFont({
+  src: "../app/fonts/Ubuntu-R.ttf",
+  variable: "--font-ubuntu-r",
+  weight: "100 900",
+});
+
+export const ubuntuBold = localFont({
+  src: "../app/fonts/Ubuntu-B.ttf",
+  variable: "--font-ubuntu-b",
+  weight: "100 900",
+});
 
 export const manropeReg = localFont({
   src: "../app/fonts/manrope-regular.otf",
