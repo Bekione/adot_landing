f922bcb7323f4f7d8a046c606aeddc0f0b525e9f
bekione <kinber024@gmail.com>
2024-12-07 06:14:46 +0300

    added product images

---
+++ data/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+    {
+      title: "Ador ERP",
+      description: "Ador ERP is a comprehensive enterprise resource planning solution tailored for Ethiopian businesses. With various modules designed to streamline processes like inventory, accounting, and HR, it’s the perfect tool to boost efficiency and productivity.",
+      link: "/products/ador-erp",
+      imageSource: "/images/adot-erp.svg", 
+      themeColor: "#4CAF50",
+    },
+    {
+      title: "Youthopia",
+      description: "Youthopia is a children’s media platform designed for both education and entertainment. It offers interactive courses, a social community for learning, and carefully crafted content to engage young minds in an immersive experience.",
+      link: "/products/youthopia",
+      imageSource: "/images/youthopia.avif",
+      themeColor: "#FF9800", 
+    },
+    {
+      title: "Coming Soon",
+      description: "Our next product is on the horizon. We’re working on innovative solutions for sectors like health and agriculture, designed to solve real-world problems and enhance lives.",
+      link: "/products/coming-soon", 
+      imageSource: "/images/coming-soon.avif", 
+      themeColor: "#9E9E9E", 
+    },
+  ];
+  
