629d1962b08f5ae0903b74d84a8af6ac0c082c64
bekione <kinber024@gmail.com>
2024-12-08 22:04:01 +0300

    added labs header

--- app/adot-labs/page.tsx
+++ app/adot-labs/page.tsx
@@ -1,9 +1,27 @@
+"use client"
+
+import Hero from '@/components/Hero'
+import { motion } from 'framer-motion'
 import React from 'react'
 
 const page = () => {
   return (
-    <div className='min-h-[110vh]'>
-      
+    <div className='min-h-screen'>
+      <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+      className="font-ubuntuReg"
+    >
+      <Hero
+        title="Adot Labs"
+        description=""
+        backgroundImage="/images/adot-lab.avif"
+        noButton
+      />
+      <div className="container mx-auto px-4">
+        </div>
+    </motion.div>
     </div>
   )
 }
