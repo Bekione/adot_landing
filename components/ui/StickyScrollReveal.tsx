8a3a58c1bdf6f89d03230dea0fc189490ae41a28
bekione <kinber024@gmail.com>
2024-12-08 22:23:14 +0300

    fix vercel lint errors

--- components/ui/StickyScrollReveal.tsx
+++ components/ui/StickyScrollReveal.tsx
@@ -4,7 +4,6 @@ import { useMotionValueEvent, useScroll } from "framer-motion";
 import { motion } from "framer-motion";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-import { ArrowRight } from "lucide-react";
 
 export const StickyScroll = ({
   content,
@@ -44,11 +43,6 @@ export const StickyScroll = ({
     setActiveCard(closestBreakpointIndex);
   });
 
-  const backgroundColors = [
-    "var(--slate-900)",
-    "var(--black)",
-    "var(--neutral-900)",
-  ];
   const linearGradients = [
     "linear-gradient(to bottom right, var(--cyan-500), var(--emerald-500))",
     "linear-gradient(to bottom right, var(--pink-500), var(--indigo-500))",
