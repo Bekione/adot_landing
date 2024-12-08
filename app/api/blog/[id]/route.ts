7110975f3cbbc5c8c6aa2e96228b0557190e76ad
bekione <kinber024@gmail.com>
2024-12-09 02:27:43 +0300

    fix: use searchParams to extract id from url

--- app/api/blog/[id]/route.ts
+++ app/api/blog/[id]/route.ts
@@ -2,24 +2,16 @@ import { NextRequest, NextResponse } from 'next/server';
 import { blogPosts } from '@/data/blogs';
 import { BlogPostRaw } from '@/types/blog';
 
-interface RouteParams {
-  params: { id: string };
-}
-
-export async function GET(
-  request: NextRequest,
-  { params }: RouteParams // Fixed type for the second argument
-) {
-  const { id } = params;
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get('id');
 
   // Find the blog post by ID
   const blogPost = (blogPosts as BlogPostRaw[]).find((post) => post.id === id);
 
-  // If no blog post is found, return a 404 response
   if (!blogPost) {
     return NextResponse.json({ message: 'Blog post not found' }, { status: 404 });
   }
 
-  // Return the found blog post as JSON
   return NextResponse.json(blogPost);
 }
