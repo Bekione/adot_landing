c441adb8f1a7edf1342b9c0c6bb9bb71dcb89012
bekione <kinber024@gmail.com>
2024-12-09 08:30:39 +0300

    update adotlabs ui

--- components/home/AdotLabsSection.tsx
+++ components/home/AdotLabsSection.tsx
@@ -1,24 +1,51 @@
+"use client"
+
 import React from "react";
-import Link from "next/link";
 import GradientWord from "../ui/GradientWord";
+import Button from "../ui/Button";
+import { motion } from "framer-motion";
+import Image from "next/image";
 
 const AdotLabsSection = () => {
   return (
-    <section className="bg-[#f4f4f4] py-16 px-4 text-center">
-      <div className="max-w-6xl mx-auto">
-        <GradientWord word="Adot Labs" />
-        <p className="text-lg text-[#555] mb-6">
-          At ADOT Labs, we support innovation through incubation and funding,
-          helping startups and tech enthusiasts bring their ideas to life. Our
-          goal is to foster creativity and drive forward-thinking solutions.
-        </p>
+    <section className="py-16 w-full overflow-hidden">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-col lg:flex-row items-center gap-12">
+          <motion.div
+            className="w-full lg:w-3/5"
+            initial={{ x: 1000, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{
+              duration: 0.8,
+              ease: [0.43, 0.13, 0.23, 0.96],
+              delay: 0.2,
+            }}
+          >
+            <GradientWord word="Adot Labs" />
+            <p className="text-lg text-gray-600 mb-6">
+              At ADOT Labs, we support innovation through incubation and
+              funding, helping startups and tech enthusiasts bring their ideas
+              to life. Our goal is to foster creativity and drive
+              forward-thinking solutions.
+            </p>
+            <Button text="See more" to="/about" className="mt-4" />
+          </motion.div>
 
-        <Link
-          href="/adot-labs"
-          className="px-8 py-2 mt-4 rounded-md bg-primary text-white font-bold font-sans transition duration-200 hover:bg-white hover:text-primary border-2 border-transparent hover:border-primary"
-        >
-          Learn More About ADOT Labs
-        </Link>
+          <motion.div
+            className="w-full lg:w-2/5"
+            initial={{ x: -1000, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.8, ease: [0.43, 0.13, 0.23, 0.96] }}
+          >
+            <Image
+              src="/images/adot-lab.avif"
+              alt="Adot Labs"
+              width={500}
+              height={300}
+              className="w-full h-auto aspect-video rounded-md shadow-lg"
+            />
+          </motion.div>
+        </div>
       </div>
     </section>
   );
