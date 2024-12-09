95e82cb823d34401d3ab6d34e925be000f7d6b01
bekione <kinber024@gmail.com>
2024-12-09 08:32:00 +0300

    added color plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,10 @@
 import type { Config } from "tailwindcss";
 
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -13,6 +18,9 @@ export default {
         foreground: "var(--foreground)",
         primary: "#503c3c",
         secondary: "#d5cea3",
+      },
+      boxShadow: {
+        input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
       },
       animation: {
         shimmer: "shimmer 2s linear infinite",
@@ -49,3 +57,15 @@ export default {
   },
   plugins: [],
 } satisfies Config;
+
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
