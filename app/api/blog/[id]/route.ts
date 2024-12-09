11b87956b3fceef84cc531d401316beeac39a223
bekione <kinber024@gmail.com>
2024-12-09 08:31:29 +0300

    removed searchparams

--- app/api/blog/[id]/route.ts
+++ app/api/blog/[id]/route.ts
@@ -2,16 +2,17 @@ import { NextRequest, NextResponse } from 'next/server';
 import { blogPosts } from '@/data/blogs';
 import { BlogPostRaw } from '@/types/blog';
 
-export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url);
-  const id = searchParams.get('id');
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
+  const { id } = params;
 
   // Find the blog post by ID
   const blogPost = (blogPosts as BlogPostRaw[]).find((post) => post.id === id);
 
+  // If no blog post is found, return a 404 response
   if (!blogPost) {
     return NextResponse.json({ message: 'Blog post not found' }, { status: 404 });
   }
 
+  // Return the found blog post as JSON
   return NextResponse.json(blogPost);
 }
