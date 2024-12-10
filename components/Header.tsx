279f35aa70fe7554f89f05cc130698ebd6a9a3cf
bekione <kinber024@gmail.com>
2024-12-10 12:30:40 +0300

    added new header and navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,65 +1,39 @@
 "use client";
-import React, { useState, useEffect } from "react";
-import { motion } from "framer-motion";
-import { Menu, MenuItem, ProductItem, HoveredLink } from "./ui/Menu";
-import { navLinks, NavLink, SubLink } from "@/data/navLinks";
+
+import * as React from "react";
+import { motion, useScroll, useMotionValueEvent } from "framer-motion";
+import { AlignJustify } from "lucide-react";
+import { NavbarMenu } from "./NavbarMenu";
+import { Sidebar } from "./Sidebar";
 import Logo from "./Logo";
-import Sidebar from "./Sidebar";
-import { AlignJustify } from 'lucide-react';
 
 const Header = () => {
-  const [isScrolled, setIsScrolled] = useState(false);
-  const [active, setActive] = useState<string | null>(null);
-  const [isSmallScreen, setIsSmallScreen] = useState(false);
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false); // Sidebar state
+  const [isScrolled, setIsScrolled] = React.useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = React.useState(false);
+  const { scrollY } = useScroll();
 
-  // Check if the screen is small
-  useEffect(() => {
-    const handleResize = () => {
-      setIsSmallScreen(window.innerWidth <= 768); // Adjust the breakpoint as needed
-    };
-
-    handleResize(); // Run on initial load
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollY = window.scrollY;
-      setIsScrolled(currentScrollY > 50); // Add class if scrolled more than 50px
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    setIsScrolled(latest > 0);
+  });
 
   return (
     <>
       <motion.header
-        className="fixed w-full rounded-md backdrop-blur-sm"
+        className="fixed w-full md:w-auto mx-auto rounded-md backdrop-blur-sm font-ubuntuReg"
         style={{ left: 0, right: 0, marginLeft: "auto", marginRight: "auto" }}
-        initial={{
-          width: "100%",
-          top: 0,
-          backgroundColor: "transparent",
-          boxShadow: "none",
-          zIndex: 999
-        }}
+        initial={{ backgroundColor: "transparent", zIndex: 998, }}
         animate={{
-          width: isScrolled ? (isSmallScreen ? "90%" : "85%") : "100%",
-          top: isScrolled ? (isSmallScreen ? "0.5rem" : "1rem") : 0,
           backgroundColor: isScrolled ? "rgba(80, 60, 60, 0.9)" : "transparent",
           boxShadow: isScrolled ? "0 2px 10px rgba(0, 0, 0, 0.1)" : "none",
-          zIndex: 9999
+          width: isScrolled ? "95%" : "100%",
+          top: isScrolled ? "1rem" : 0,
+          zIndex: 9998,
         }}
         transition={{ duration: 0.4, ease: "easeInOut" }}
       >
-        <div className="p-2 sm:px-4 flex justify-between items-center">
-          {/* Logo */}
+        <div className="mx-auto px-4 py-4 flex justify-between items-center">
           <Logo />
-
-          {/* Menu Button */}
+          <NavbarMenu />
           <button
             className="p-2 rounded-md text-[#d5cea3] md:hidden"
             onClick={() => setIsSidebarOpen(true)}
@@ -67,40 +41,8 @@ const Header = () => {
           >
             <AlignJustify size={32} />
           </button>
-
-          {/* Navigation Menu */}
-          <Menu setActive={setActive}>
-            {navLinks.map((navLink: NavLink) => (
-              <MenuItem
-                key={navLink.title}
-                setActive={setActive}
-                active={active}
-                item={navLink.title}
-                href={navLink.href}
-                hasDropdown={["Services", "Products"].includes(navLink.title)} // Enable dropdown only for specific links
-              >
-                {navLink.subLinks?.map((subLink: SubLink) =>
-                  subLink.src ? (
-                    <ProductItem
-                      key={subLink.title}
-                      title={subLink.title}
-                      description={subLink.description || ""}
-                      href={subLink.href}
-                      src={subLink.src}
-                    />
-                  ) : (
-                    <HoveredLink key={subLink.title} href={subLink.href}>
-                      {subLink.title}
-                    </HoveredLink>
-                  )
-                )}
-              </MenuItem>
-            ))}
-          </Menu>
         </div>
       </motion.header>
-
-      {/* Sidebar */}
       <Sidebar isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />
     </>
   );
--- data/navLinks.ts
+++ data/navLinks.ts
@@ -28,20 +28,12 @@ export const navLinks: NavLink[] = [
     title: "Products",
     href: "/products",
     subLinks: [
-      { title: "Adot ERP", description: "Manage your enterprise efficiently.", href: "/products/adot-erp", src: "/images/adot-erp.svg" },
-      { title: "Youthopia", description: "Kids number one choice.", href: "/products/youthopia", src: "/images/youthopia.avif" },
-      { title: "Comming Soon", description: "Something exciting on it way.", href: "/products/comming-soon", src: "/images/coming-soon.avif" },
+      { title: "Adot ERP", description: "Manage your enterprise efficiently.", href: "/products/adot-erp", src: "/images/products/adot-erp/adot-erp.svg" },
+      { title: "Youthopia", description: "Kids number one choice.", href: "/products/youthopia", src: "/images/products/youthopia/youthopia-snap-1.png" },
+      { title: "Comming Soon", description: "Something exciting on it way.", href: "/products/coming-soon", src: "/images/products/coming-soon.avif" },
     ],
   },
-  { title: "Adot Labs", href: "/adot-labs" },
   { title: "Blog", href: "/blog" },
   { title: "Contact Us", href: "/contact" },
 ];
-
-
-
-
-
-
-
 
