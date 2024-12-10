26463ea764fc7eadfa8540fe0a522e17810fc072
bekione <kinber024@gmail.com>
2024-12-10 12:38:19 +0300

    changed active button color

--- components/ui/Tabs.tsx
+++ components/ui/Tabs.tsx
@@ -62,7 +62,7 @@ export const Tabs = ({
                 layoutId="clickedbutton"
                 transition={{ type: "spring", bounce: 0.3, duration: 0.6 }}
                 className={cn(
-                  "absolute inset-0 bg-secondary rounded-full",
+                  "absolute inset-0 bg-primary rounded-full",
                   activeTabClassName
                 )}
               />
