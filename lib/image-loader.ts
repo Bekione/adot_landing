e2eb7d5eeba32e1e0200052fb108ebed1bed9c3f
bekione <kinber024@gmail.com>
2024-12-13 11:55:03 +0300

    feat: add blur placeholder function for Cloudinary images

--- lib/image-loader.ts
+++ lib/image-loader.ts
@@ -1,20 +1,21 @@
 const CLOUDINARY_BASE_URL = process.env.NEXT_PUBLIC_CLOUDINARY_BASE_URL;
 
 type CloudinaryLoaderProps = {
-    src: string;
-    width: number;
-    quality?: number | string;
-  };
-  
-  export default function cloudinaryLoader({
-    src,
-    width,
-    quality,
-  }: CloudinaryLoaderProps): string {
-    const params = ["f_auto", "c_limit", `w_${width}`, `q_${quality || "auto"}`];
-  
-    return `${CLOUDINARY_BASE_URL}/${params.join(
-      ","
-    )}/adot/${src}`;
-  }
-  
+  src: string;
+  width: number;
+  quality?: number | string;
+};
+
+export default function cloudinaryLoader({
+  src,
+  width,
+  quality,
+}: CloudinaryLoaderProps): string {
+  const params = ["f_auto", "c_limit", `w_${width}`, `q_${quality || "auto"}`];
+
+  return `${CLOUDINARY_BASE_URL}/${params.join(",")}/adot/${src}`;
+}
+
+export function cloudinaryBlurPlaceholder(src: string): string {
+  return `${CLOUDINARY_BASE_URL}/f_auto,q_10,w_10/adot/${src}`; // Super low quality and small size
+}
