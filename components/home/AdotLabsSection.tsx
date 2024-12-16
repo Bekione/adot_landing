a4ed45b04b6d96e8944edabf4c7757112a6abab7
bekione <kinber024@gmail.com>
2024-12-16 22:45:06 +0300

    chore: update framer div to include variant transitions

--- components/home/AdotLabsSection.tsx
+++ components/home/AdotLabsSection.tsx
@@ -3,19 +3,28 @@
 import React from "react";
 import GradientWord from "../ui/GradientWord";
 import Button from "../ui/Button";
-import { motion } from "framer-motion";
+import { motion, useInView } from "framer-motion";
 import Image from "next/image";
 import cloudinaryLoader, { cloudinaryBlurPlaceholder } from "@/lib/image-loader";
 
 const AdotLabsSection = () => {
+  const variants = {
+    hiddenLeft: { x: "-100%", opacity: 0 },
+    hiddenRight: { x: "100%", opacity: 0 },
+    visible: { x: 0, opacity: 1 },
+  };
+
   return (
     <section className="my-10 pt-16 pb-10 w-full overflow-hidden">
-      <div className="container mx-auto px-4  md:w-11/12 lg:w-10/12">
+      <div className="container mx-auto px-4 md:w-11/12 lg:w-10/12">
         <div className="flex flex-col lg:flex-row items-center gap-12 mx-8 md:mx-0">
+          {/* Left Section */}
           <motion.div
             className="w-full lg:w-3/5"
-            initial={{ x: 1000, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
+            initial="hiddenRight"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.1 }}
+            variants={variants}
             transition={{
               duration: 0.8,
               ease: [0.43, 0.13, 0.23, 0.96],
@@ -32,10 +41,13 @@ const AdotLabsSection = () => {
             <Button text="See more" to="/about" className="mt-4" />
           </motion.div>
 
+          {/* Right Section */}
           <motion.div
             className="w-full lg:w-2/5"
-            initial={{ x: -1000, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
+            initial="hiddenLeft"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.1 }}
+            variants={variants}
             transition={{ duration: 0.8, ease: [0.43, 0.13, 0.23, 0.96] }}
           >
             <div className="w-full flex justify-center">
