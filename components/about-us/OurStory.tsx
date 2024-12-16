fbe4863fac6cd807feba32e85a9fc129df48b6d5
bekione <kinber024@gmail.com>
2024-12-16 22:55:14 +0300

    style: add overflow-hidden to  hide page overflow

--- components/about-us/OurStory.tsx
+++ components/about-us/OurStory.tsx
@@ -11,7 +11,7 @@ export default function OurStory() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.6, delay: 0.6 }}
-      className="my-16 flex flex-col items-center md:w-11/12"
+      className="my-16 flex flex-col items-center md:w-11/12 overflow-hidden"
     >
       <GradientWord word="Our Story" />
       <Timeline data={ourStory} />
