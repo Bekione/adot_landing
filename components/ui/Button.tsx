58f81688c118f0471fa5d709b4b3bfceff483790
bekione <kinber024@gmail.com>
2024-12-18 02:50:59 +0300

    refactor: enhance Button component styles for improved consistency and responsiveness

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -32,11 +32,11 @@ const Button = ({
         target={target} 
         rel={target ? "noopener noreferrer" : undefined}
         className={cn(
-          className,
-          'font-ubuntuReg',
+          'font-ubuntuReg relative inline-block px-6 py-2 rounded-lg transition-all duration-300 overflow-hidden z-50',
           type === "default"
-            ? "relative inline-block px-6 py-2 rounded-lg text-white bg-primary hover:bg-secondary hover:text-primary transition-all duration-300 overflow-hidden z-50"
-            : "relative inline-flex w-fit h-12 animate-shimmer items-center justify-center rounded-md border border-primary bg-[linear-gradient(110deg,#503c3c,45%,#704d4d,55%,#503c3c)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors hover:shadow-2xl hover:shadow-primary/[0.3] overflow-hidden z-50"
+            ? "text-white bg-primary hover:bg-secondary hover:text-primary"
+            : "w-fit h-12 animate-shimmer items-center justify-center rounded-md border border-primary bg-[linear-gradient(110deg,#503c3c,45%,#704d4d,55%,#503c3c)] bg-[length:200%_100%] text-white hover:shadow-2xl hover:shadow-primary/[0.3]",
+          className
         )}
       >
         <Noise />
