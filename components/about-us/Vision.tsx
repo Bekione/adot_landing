351b86b933a604ccbc86ba928760f377c94c00e1
bekione <kinber024@gmail.com>
2024-12-18 02:46:10 +0300

    fix: correct HTML entities for apostrophes in Vision component text

--- components/about-us/Vision.tsx
+++ components/about-us/Vision.tsx
@@ -10,9 +10,9 @@ export default function Vision() {
     >
       <h3 className="text-2xl font-semibold font-ubuntuBold mb-4">Our Vision</h3>
       <p className="text-justify">
-        We aspire to become Africa's most trusted technology partner, shaping the future of innovation. 
+        We aspire to become Africa&apos;s most trusted technology partner, shaping the future of innovation. 
         Our aim is to make modern technology and software accessible and affordable for everyone, 
-        breaking down barriers to entry for digital solutions. We envision building the continent's 
+        breaking down barriers to entry for digital solutions. We envision building the continent&apos;s 
         largest research center, empowering youth to drive change, and expanding globally.
       </p>
     </motion.div>
