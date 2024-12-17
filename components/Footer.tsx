128062163c6c98f52374c64d17180079c9425676
bekione <kinber024@gmail.com>
2024-12-18 02:44:45 +0300

    refactor: remove unused delay import from Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { delay, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import Link from "next/link";
 import Image from "next/image";
 import { blogPosts } from "@/data/blogs";
