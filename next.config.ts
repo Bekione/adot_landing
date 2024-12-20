3ff14fdff05a6cb1d47cc488f1d20cb68551360d
bekione <kinber024@gmail.com>
2024-12-20 18:23:52 +0300

    feat: add custom headers and caching strategy for optimized image delivery in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,49 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
-    loader: "custom",
+    loader: 'custom',
     loaderFile: "./lib/image-loader.ts",
+  },
+  async headers() {
+    return [
+      {
+        source: '/:all*(svg|jpg|png|webp|avif)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        source: '/_next/static/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        source: '/_next/image/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        // For Analytics scripts
+        source: '/va/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, stale-while-revalidate=31536000',
+          },
+        ],
+      },
+    ];
   },
 };
 
