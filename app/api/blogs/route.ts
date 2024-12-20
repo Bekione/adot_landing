e2112d159bb66d4e5a4e45d7e84b99403cab0b1c
bekione <kinber024@gmail.com>
2024-12-20 18:24:07 +0300

    feat: add revalidation interval for blog posts to enhance caching strategy

--- app/api/blogs/route.ts
+++ app/api/blogs/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server'
 import { blogPosts } from '@/data/blogs'
 import { BlogPostRaw } from '@/types/blog'
 
+export const revalidate = 3600 // Revalidate every hour
+
 export async function GET() {
   return NextResponse.json(blogPosts as BlogPostRaw[])
 }
